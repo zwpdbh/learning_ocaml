3e86110581b329c251485c7d4b6bfef307b8ca98
zwpdbh <hyperion_z@outlook.com>
2021-06-05 17:11:28 +0800

    >

--- variant_types.ml
+++ variant_types.ml
@@ -32,3 +32,42 @@ let rec evaluate e =
 
 (* 1 + 2 ∗ 3 *)
 evaluate (Add (Num 1, Multiply (Num 2, Num 3)))
+
+
+
+type point  = float * float;;
+type shape =
+  | Point  of point
+  | Circle of point * float (* center and radius *)
+  | Rect   of point * point (* lower-left and
+                               upper-right corners *);;
+
+
+type number =
+  | Zero
+  | Integer of int
+  | Real of float ;;
+
+let zero = Zero;;
+let i = Integer 1;;
+let x = Real 3.2;;
+
+let float_of_numer = function
+  | Zero -> 0.0
+  | Integer i -> float_of_int i
+  | Real x -> x;;
+
+let add n1 n2 =
+  match n1, n2 with
+  | Zero, n
+    | n, Zero ->
+     n
+  | Integer i1, Integer i2 ->
+     Integer (i1 + i2)
+  | Integer i, Real x
+    | Real x, Integer i ->
+     Real (x +. float_of_int i)
+  | Real x1, Real x2 ->
+     Real (x1 +. x2);;
+
+add x i;;
